16998c6d687db64dcb929b26f947468f7bf01ee9
israt-afroz <isratafroz55@gmail.com>
2022-05-15 22:37:43 +0600

    modified branch_feature from feature2

--- app/src/test/java/Branch/Branch_feature.java
+++ app/src/test/java/Branch/Branch_feature.java
@@ -2,9 +2,17 @@ package Branch;
 
 public class Branch_feature {
     String name,id;
+ double inc;
+    public Branch_feature(String id,String name,double inc) {
+        this.id = id; this.name = name; this.inc=inc;
+    }
 
-    public Branch_feature(String id,String name) {
-        this.id = id; this.name = name;
+    public double getInc() {
+        return inc;
+    }
+
+    public void setInc(double inc) {
+        this.inc = inc;
     }
 
     public String getId() {
